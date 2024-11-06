d230854dd45a777ccb70e9c79be5ab1588a70f05
norninha do grau <106700570+moyses17@users.noreply.github.com>
2024-11-06 15:45:38 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400,WEBGL);
+  terra=loadImage('terra.jpg')
+}
+
+function draw() {
+  background(310,202,120); //cor de fundo
+  rotateY(millis()/1000) //rotaçãon da terra
+  texture(terra); //textura da terra, Imagem.
+  sphere(140, 160); //esfera 3D
+
